7d6661183ca8e492f611dd5ada4a39287c01c888
Siyalov <91375207+Siyalov@users.noreply.github.com>
2022-11-26 17:35:02 +0200

    pagination, card image styles

--- src/Pages/Catalog.tsx
+++ src/Pages/Catalog.tsx
@@ -4,19 +4,27 @@ import Card from "../components/Card";
 
 export default function Catalog() {
    const [products, setProducts] = useState<ProductsResponse>();
+   const [page, setPage] = useState(0);
 
    async function loadProducts() {
-      const data = await api.getProducts();
+      const data = await api.getProducts({
+         page: page,
+         pageSize: 40,
+      });
       setProducts(data);
    }
 
    useEffect(() => {
       loadProducts();
-   }, []);
+   }, [page]);
 
 
    return <>
       {products?.page}<br/>
+      <div>
+         <button onClick={() => setPage(Math.max(page - 1, 0))}>назад</button>
+         <button onClick={() => setPage(page + 1)}>вперед</button>
+      </div>
       <div className="cards-container">
          {products?.entries
             ? products?.entries.map(product => 
