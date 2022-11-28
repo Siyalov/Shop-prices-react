30b25444125faab5a51bec4ffcbf2287e9e6ea2a
Siyalov <91375207+Siyalov@users.noreply.github.com>
2022-11-28 23:09:20 +0200

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Catalog from './Pages/Catalog';
+import { Routes, Route } from 'react-router-dom';
 
 import "./index.css" 
 import Header from './components/Header';
