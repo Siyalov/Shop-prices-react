c9326556bdf1146a745865cd1ed613e3db95dec9
Siyalov <91375207+Siyalov@users.noreply.github.com>
2023-01-04 20:03:02 +0200

    favorites nav button

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,13 @@
 import React, { useContext } from "react";
 import logo from "./img/Cittaslow_logo.png";
 import "./style.css";
-import { BoxArrowDownLeft, BoxArrowInDownLeft, PersonPlusFill } from "react-bootstrap-icons";
+import { BoxArrowDownLeft, BoxArrowInDownLeft, HeartFill, PersonPlusFill } from "react-bootstrap-icons";
 import { Link } from "react-router-dom";
 import { Context } from "../../App";
 import api from "../../Api/api";
 
 export default function Header() {
-  const { searchQuery, setSearchQuery, setToken, user } = useContext(Context);
+  const { searchQuery, setSearchQuery, setToken, user, favorites } = useContext(Context);
 
   const handler: React.ChangeEventHandler<HTMLInputElement> = (e) => {
     setSearchQuery(e.target.value);
@@ -36,6 +36,10 @@ export default function Header() {
       <input type="search" value={searchQuery} onChange={handler} />
       <div className="buttons">
         <nav className="header-nav">
+          {user ? <Link to={"/favorites"}>
+            <HeartFill />
+            <span>{favorites.length}</span>
+          </Link> : '' }
           {!user ? <Link to={"/register"}>
             <PersonPlusFill />
           </Link> : '' }
