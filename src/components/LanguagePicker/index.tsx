55c2574798435a7818e71effe0a3b50cbc4bc1e1
Rtif173 <13.meilleur@gmail.com>
2023-01-08 15:15:50 +0200

    Header style fix: * size of input and logo * style of language picker

--- src/components/LanguagePicker/index.tsx
+++ src/components/LanguagePicker/index.tsx
@@ -7,7 +7,7 @@ import { supportedLanguages, supportedLanguagesFullNames, flags } from "../../i1
 
 export default function LanguagePicker() {
   const { t, i18n } = useTranslation();
-  const [ isOpen, setIsOpen ] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
   function switchSelectionBox() {
     setIsOpen(!isOpen);
@@ -15,16 +15,13 @@ export default function LanguagePicker() {
 
   return <>
     {isOpen ? <div className="language-picker-choices">
-      {supportedLanguages.map(e => {
-        return <div className="clickable" onClick={() => i18n.changeLanguage(e)}>
-          <img
-            width={24}
-            src={
-              // @ts-ignore TODO: fix
-              flags[e] || ''
-            }
-            alt={e}/>
-            {supportedLanguagesFullNames[e]}
+      {supportedLanguages.map(lang => {
+        return <div className={"clickable language-list-item"+ (i18n.language == lang ? " active":"")} onClick={() => i18n.changeLanguage(lang)}>
+          <div className="flag" style={{
+            backgroundImage: `url("${flags[lang]}")`
+          }}>
+          </div>
+          <span className="language-list-item-name">{supportedLanguagesFullNames[lang]}</span>
         </div>
       })}
     </div> : ''}
@@ -32,12 +29,13 @@ export default function LanguagePicker() {
       <Globe />
       <span>
         <img
-          height={24}
+          className="header-flag"
+          height={20}
           src={
             // @ts-ignore TODO: fix
             flags[i18n.language] || ''
           }
-          alt={i18n.language}/>
+          alt={i18n.language} />
       </span>
     </a>
   </>
