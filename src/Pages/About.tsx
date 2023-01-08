392278710ff51301f99edafe0a1450e150d19417
Siyalov <91375207+Siyalov@users.noreply.github.com>
2023-01-08 23:54:38 +0200

    About pages update

--- src/Pages/About.tsx
+++ src/Pages/About.tsx
@@ -25,7 +25,7 @@ export default function About() {
       <Col xs={12} >
         <h1><Github size={32}/> Github:</h1>
         <a href="https://github.com/Siyalov/Shop-prices-react">
-          <img alt="GitHub OpenGraph statistics" height="300px" src="https://opengraph.githubassets.com/9c370dce82aa0f8d2db791db8649d42e31b7a8ea2fab257ba8b04265144c973f/Siyalov/Shop-prices-react"/>
+          <img alt="GitHub OpenGraph statistics" height="300px" src={`https://opengraph.githubassets.com/${Math.random()}/Siyalov/Shop-prices-react`}/>
         </a>
       </Col>
       <Col xs={12} style={{textAlign: "center"}}><h2>{t('aboutProject.developers')}</h2></Col>
